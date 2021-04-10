d422218e22f8d9d6dbb6dcb9c1a3cfafafebbfb7
Yoshida-Eduardo <edry97@hotmail.com>
2021-04-10 13:27:18 -0300

    Moves docker-compose to software_gateway, as it'll be the main app entrypoint

--- config/config.js
+++ config/config.js
@@ -19,5 +19,24 @@ module.exports = {
       min: 0,
       idle: 10000
     }
+  },
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    operatorsAliases: Sequelize.Op,
+    logging: false,
+    quoteIdentifiers: false,
+    keepDefaultTimezone: true,
+    define: {
+      timestamps: true
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
   }
 }
